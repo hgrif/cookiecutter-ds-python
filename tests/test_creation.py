ea7eeb61060ed39d956902ed3575fe42732b6aa9
eli knaap <ek@knaaptime.com>
2020-03-13 14:13:28 -0700

    fix test

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -86,5 +86,5 @@ def no_curlies(filepath):
 def test_paper(default_baked_project):
     """Test whether the paper gets compiled."""
     os.system(f" cd {default_baked_project}; make paper")
-    paper_path = os.path.join(default_baked_project, f"paper/compiled/{default_baked_project}.pdf")
+    paper_path = os.path.join(default_baked_project, f"paper/compiled/project_name.pdf")
     assert os.path.exists(paper_path)
