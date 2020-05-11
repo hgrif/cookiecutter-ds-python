0470f65a0f64bb688361e8c7766560097ba02341
eli knaap <ek@knaaptime.com>
2020-05-11 13:40:34 -0700

    add amsmath package to template

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.python_module_name }}/util.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.python_module_name }}/util.py
@@ -6,7 +6,16 @@ import pandas as pd
 from tabulate import tabulate
 
 
-def save_table(df, path=None, type="pipe", landscape=False, caption=None, ref=None, tabulate_kws=None):
+def save_table(
+    df,
+    path=None,
+    type="pipe",
+    landscape=False,
+    caption=None,
+    ref=None, 
+    resize=True,
+    tabulate_kws=None,
+):
     """Save a pandas dataframe as a markdown table.
 
     Parameters
@@ -24,6 +33,8 @@ def save_table(df, path=None, type="pipe", landscape=False, caption=None, ref=No
         the table's caption
     ref : str
         short name used to reference the table with pandoc-crossref
+    resize: bool
+        whether the table should be resized to fit on a single page
     tabulate_kws : dict
         additional keyword arguments passed to tabulate
 
@@ -33,23 +44,28 @@ def save_table(df, path=None, type="pipe", landscape=False, caption=None, ref=No
     if not tabulate_kws:
         tabulate_kws = {}
 
-
     if landscape:
         print("\\begin{landscape}\n")
+
     if type == "latex":
-        print(
-            "\\begin{table}[h]\n"
-            "\\begin{center}" + tabulate(df, tablefmt=type, headers="keys", **tabulate_kws) + "\n"
-        )
-    else: 
+        print("\\begin{table}[h]\n")
+        print("\\begin{center}")
+        if resize:
+            print("\\resizebox{\\linewidth}{!}{")
+            print(tabulate(df, tablefmt=type, headers="keys", **tabulate_kws) + "\n" + "}")
+        else:
+            print(tabulate(df, tablefmt=type, headers="keys", **tabulate_kws) + "\n")
+
+    else:
         print(tabulate(df, tablefmt=type, headers="keys"))
-        
     if caption and type == "latex":
-        print("\n\\caption{"+f"{caption}"+"}" +"\n" + "\label{tbl:"+f"{ref}"+"}"
-)
+        print(
+            "\n\\caption{" + f"{caption}" + "}" + "\n" + "\\label{tbl:" + f"{ref}" + "}"
+        )
     elif caption:
         print(f"\n: {caption}" + " {#tbl:" + f"{ref}" + "}")
-    if type=='latex':
+
+    if type == "latex":
         print("\\end{center}\n" "\\end{table}\n")
 
     if landscape:
